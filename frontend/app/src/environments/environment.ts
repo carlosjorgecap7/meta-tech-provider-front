c9315caaa4608da044a507af2cf70c8598b02020
Carlos Jorge <carlos.jorge-chaves@capgemini.com>
2026-07-23 14:02:50 +0200

    fix: remove process.env from environment, keep values empty for user input

--- frontend/app/src/environments/environment.ts
+++ frontend/app/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  metaAppId: process.env['NG_APP_META_APP_ID'] || '',
-  configId: process.env['NG_APP_CONFIG_ID'] || '',
-  redirectUri: process.env['NG_APP_REDIRECT_URI'] || '',
-  clientId: process.env['NG_APP_CLIENT_ID'] || '',
-  apiUrl: process.env['NG_APP_API_URL'] || '',
+  metaAppId: '',
+  configId: '',
+  redirectUri: '',
+  clientId: '',
+  apiUrl: '',
 };
