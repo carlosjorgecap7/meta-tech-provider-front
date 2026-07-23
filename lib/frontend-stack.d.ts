a1e3ecab989e46304504a65007627405b01f1a31
Carlos Jorge <carlos.jorge-chaves@capgemini.com>
2026-07-23 12:47:56 +0200

    chore: align front repo with managed CDK setup and clean TS build outputs

--- lib/frontend-stack.d.ts
+++
@@ -1,8 +0,0 @@
-import * as cdk from "aws-cdk-lib";
-import { Construct } from "constructs";
-export interface MetaTechProviderFrontendStackProps extends cdk.StackProps {
-    entorno: string;
-}
-export declare class MetaTechProviderFrontendStack extends cdk.Stack {
-    constructor(scope: Construct, id: string, props: MetaTechProviderFrontendStackProps);
-}
